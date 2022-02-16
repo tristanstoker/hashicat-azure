21e573b82b3c2a3a49d2061286f7d14d0207ca1a
tristanstoker <tristan@hashicorp.com>
2022-02-16 16:27:38 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -110,6 +110,10 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
+  
+  tags = {
+    Department = "devops"
+  }
 
   storage_image_reference {
     publisher = var.image_publisher
